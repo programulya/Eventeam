60b3a92a279b1816984688e8934ef2dd6121b42c
Julia Savinkova <julsavinkova@gmail.com>
2015-06-30 00:54:39 +0300

    Fix counters scroll

--- Eventeam/Scripts/home.js
+++ Eventeam/Scripts/home.js
@@ -1,19 +1,30 @@
-﻿$('#countries').jQuerySimpleCounter({
-    end: 7,
-    duration: 7000
-});
+﻿var home = (function() {
+    var isLoaded = false;
 
-$('#cities').jQuerySimpleCounter({
-    end: 28,
-    duration: 7000
-});
+    $(document).on("scroll", function() {
 
-$('#concepts').jQuerySimpleCounter({
-    end: 267,
-    duration: 7000
-});
+        if ($(this).scrollTop() >= $('#services').position().top && isLoaded === false) {
+            $('#countries').jQuerySimpleCounter({
+                end: 7,
+                duration: 7000
+            });
 
-$('#projects').jQuerySimpleCounter({
-    end: 365,
-    duration: 7000
-});
+            $('#cities').jQuerySimpleCounter({
+                end: 28,
+                duration: 7000
+            });
+
+            $('#concepts').jQuerySimpleCounter({
+                end: 267,
+                duration: 7000
+            });
+
+            $('#projects').jQuerySimpleCounter({
+                end: 365,
+                duration: 7000
+            });
+
+            isLoaded = true;
+        }
+    });
+})();
