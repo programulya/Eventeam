c46fd08f6859bada821676be8c1e8b6bb342736d
Julia Savinkova <julsavinkova@gmail.com>
2015-06-30 00:25:52 +0300

    Add counters

--- Eventeam/Scripts/contact.js
+++ Eventeam/Scripts/contact.js
@@ -1,15 +1,3 @@
-/*------------------------------------------------------------------
-Project:    Paperclip
-Author:     Andrey
-Owner:      Yevgeny Simzikov
-URL:        http://simpleqode.com/
-            https://twitter.com/YevSim
-Version:    1.2.0
-Created:        11/03/2014
-Last change:    18/02/2015
--------------------------------------------------------------------*/
-
-
 /**
  * Contact form
  */
---
+++ Eventeam/Scripts/home.js
@@ -0,0 +1,19 @@
+﻿$('#countries').jQuerySimpleCounter({
+    end: 7,
+    duration: 7000
+});
+
+$('#cities').jQuerySimpleCounter({
+    end: 28,
+    duration: 7000
+});
+
+$('#concepts').jQuerySimpleCounter({
+    end: 267,
+    duration: 7000
+});
+
+$('#projects').jQuerySimpleCounter({
+    end: 365,
+    duration: 7000
+});
