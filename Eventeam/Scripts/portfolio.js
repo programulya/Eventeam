2a20f6c0d5b4bf7d91df1afe1f5e8893020f7a45
Yuri <yuri.kushch@gmail.com>
2015-11-29 19:53:29 +0200

    Fix gallery pagination

--- Eventeam/Scripts/portfolio.js
+++ Eventeam/Scripts/portfolio.js
@@ -9,60 +9,95 @@
  */
 
 // init Isotope
-var $container = $('#isotope-container').imagesLoaded(function() {
+var $container = $('#isotope-container').imagesLoaded(function () {
     $container.isotope({
         itemSelector: '.isotope-item',
         layoutMode: 'fitRows'
     });
 });
 // filter items on button click
-$('#filters a').on('click', function() {
+$('#filters a').on('click', function () {
     var filterValue = $(this).attr('data-filter');
     $container.isotope({ filter: filterValue });
     return false;
 });
 
-$(document).ready(function() {
-    var galleryList = model.GalleryPhotoList;
-    var itemsOnPage = 8;
+$(document).ready(function () {
+    var galleryList = model.GalleryPhotoList,
+        imgList = [],
+        itemsOnPage = 8;
     var lastItemIndex = galleryList.length - 1 < itemsOnPage
         ? galleryList.length - 1
         : itemsOnPage - 1;
     var defaultOptions = {
         firstItemIndex: 0,
-        lastItemIndex: lastItemIndex
+        lastItemIndex: lastItemIndex,
+        maxItemsOnPage: 8
     }
 
-    var getGalleryContent = function(options) {
+    var getGalleryContent = function (options) {
         var galleryContent = '';
-        for (var i = options.firstItemIndex, size = options.lastItemIndex; i <= size; i++) {
-            var porfolioItem = '<div class="col-sm-3">' +
-                '<div class="portfolio-item">' +
-                '<div class="portfolio-thumbnail">' +
-                '<img class="img-responsive" src="' + prepareLink(galleryList[i].LinkResponsive) + '" alt="' + galleryList[i].Alt + '">' +
-                '<div class="mask">' +
-                '<p class="zoom-in">' +
-                '<a href="' + prepareLink(galleryList[i].Link) + '" data-lightbox="template_showcase"><i class="fa fa-search-plus fa-2x"></i></a>' +
-                '</p>' +
-                '</div>' +
-                '</div>' +
-                '</div>' +
-                '</div>';
-            galleryContent += porfolioItem;
+        for (var i = options.firstItemIndex, size = galleryList.length - 1; i <= size; i++) {
+            var defaultOptions = i < options.maxItemsOnPage
+                ? {}
+                : { style: 'style="display: none;"' };
+            var portfolioOpt = $.extend(defaultOptions, {
+                linkResponsive: galleryList[i].LinkResponsive,
+                alt: galleryList[i].Alt,
+                href: galleryList[i].Link
+            });
+            
+            galleryContent += generatePhotoItem(portfolioOpt);
         }
+
         return galleryContent;
     }
+
+    function generatePhotoItem(options) {
+        var opt = $.extend({}, options);
+        return '<div class="col-sm-3 portfolio-photo"' + opt.style + '>' +
+                    '<div class="portfolio-item">' +
+                    '<div class="portfolio-thumbnail">' +
+                    '<img class="img-responsive" src="' + prepareLink(opt['linkResponsive']) + '" alt="' + opt['alt'] + '">' +
+                    '<div class="mask">' +
+                    '<p class="zoom-in">' +
+                    '<a href="' + prepareLink(opt['href']) + '" data-lightbox="template_showcase"><i class="fa fa-search-plus fa-2x"></i></a>' +
+                    '</p>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>';
+    }
+
     $('#photo-gallery').html(getGalleryContent(defaultOptions));
 
     $('#page-selection').bootpag({
         total: Math.ceil(galleryList.length / itemsOnPage)
-    }).on('page', function(event, num) {
-        var lastItemIndex = itemsOnPage * num - 1;
+    }).on('page', function (event, num) {
+        // do this here due to possibility of not full load of content
+        if (!imgList.length > 0) {
+            $('div.portfolio-photo').each(function (index, value) {
+                imgList.push(value);
+            });
+        }
+        removePhotoContent();
+        var options = $.extend({}, defaultOptions, getModifiedOptions({
+            itemsOnPage: itemsOnPage,
+            num: num,
+            content: galleryList
+        }));
+        for (var i = options.firstItemIndex; i <= options.lastItemIndex; i++) {
+            $(imgList[i]).css({ 'display': 'inline-block' });
+        }
+    });
+
+    function getModifiedOptions(pageOpt) {
+        var lastItemIndex = pageOpt.itemsOnPage * pageOpt.num - 1;
         var modifiedOptions;
 
-        if (galleryList.length < lastItemIndex) {
-            lastItemIndex = galleryList.length - 1;
-            var previousPageLastIndex = itemsOnPage * (num - 1);
+        if (pageOpt.content.length < lastItemIndex) {
+            lastItemIndex = pageOpt.content.length - 1;
+            var previousPageLastIndex = pageOpt.itemsOnPage * (pageOpt.num - 1);
             modifiedOptions = {
                 lastItemIndex: lastItemIndex,
                 firstItemIndex: previousPageLastIndex
@@ -70,11 +105,13 @@ $(document).ready(function() {
         } else {
             modifiedOptions = {
                 lastItemIndex: lastItemIndex,
-                firstItemIndex: lastItemIndex - (itemsOnPage - 1)
+                firstItemIndex: lastItemIndex - (pageOpt.itemsOnPage - 1)
             };
         }
+        return modifiedOptions;
+    }
 
-        var options = $.extend({}, defaultOptions, modifiedOptions);
-        $('#photo-gallery').html(getGalleryContent(options));
-    });
+    function removePhotoContent() {
+        $('div.portfolio-photo:visible').css({ 'display': 'none' });
+    }
 });
