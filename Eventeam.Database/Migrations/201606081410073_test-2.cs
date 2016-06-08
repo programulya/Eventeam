4e86d04403d50896814d3083a973818c31875094
Yuri <yuri.kushch@gmail.com>
2016-06-08 17:26:39 +0300

    Test migration hacks

---
+++ Eventeam.Database/Migrations/201606081410073_test-2.cs
@@ -0,0 +1,16 @@
+namespace Eventeam.Database.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class test2 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
